19fcfc912ea175ee8bfe8507784fd49cc34100d3
01407273 <chenhao37@sfmail.sf-express.com>
2023-02-19 13:49:50 +0800

    初始经

---
+++ src/main/scala/spark/sql/Person.scala
@@ -0,0 +1,17 @@
+package spark.sql
+
+/**
+ * <pre>
+ * *********************************************
+ * Copyright sf-express.
+ * All rights reserved.
+ * Description: 
+ * HISTORY
+ * *********************************************
+ * ID   DATE               PERSON             REASON
+ * 1   2023/2/17 16:47     01407273            Create
+ *
+ * *********************************************
+ * </pre>
+ */
+case class Person(id: Long, name: String)
