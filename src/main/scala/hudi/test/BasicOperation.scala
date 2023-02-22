f96c2200404d897fe99cf672163a30ca50ba26a6
01407273 <chenhao37@sfmail.sf-express.com>
2023-02-22 09:40:36 +0800

    learn

---
+++ src/main/scala/hudi/test/BasicOperation.scala
@@ -0,0 +1,38 @@
+package hudi.test
+
+import org.apache.hudi.QuickstartUtils.DataGenerator
+import org.apache.spark.sql.SparkSession
+import org.scalatest.funsuite.AnyFunSuite
+
+/**
+ * <pre>
+ * *********************************************
+ * Copyright sf-express.
+ * All rights reserved.
+ * Description: 
+ * HISTORY
+ * *********************************************
+ * ID   DATE               PERSON             REASON
+ * 1   2023/2/20 10:32     01407273            Create
+ *
+ * *********************************************
+ * </pre>
+ */
+class BasicOperation   extends AnyFunSuite{
+ // val dataGen = new DataGenerator
+  val spark = SparkSession.builder()
+    .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+    .config("spark.sql.extensions", "org.apache.spark.sql.hudi.HoodieSparkSessionExtension")
+    .config("schema.on.read.enable",true)
+    .config("spark.memory.offHeap.enabled",true)
+    .config("spark.memory.offHeap.size","1g")
+    .master("local[*]").getOrCreate()
+  val tableName = "hudi_trips_cow1"
+  var basePath = "file:///G:\\huditmp\\hudi1"
+  import spark.implicits._
+
+  test("read"){
+    val frame = spark.read.format("hudi").load(basePath)
+    frame.show(1000)
+  }
+}
